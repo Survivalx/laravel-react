885d3a44eedb66c7dcecb66545cbe99d2c50e946
Survivalx <goshaz7c@icloud.com>
2024-04-06 10:17:10 +0300

    react-router-dom

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 
 const Layout = () => {
     return (
         <>
             <header className='flex justify-between px-5 py-3 items-center border-blue-500'>
                 <Link className='text-xl font-bold text-blue-500' to='/'>Cars.md</Link>
-            </header>
             <nav className='flex gap-3'>
                 <Link to='/cars'>Cars</Link>
                 <Link to='/new-car'>New car</Link>
             </nav>
+            </header>
             <main className='px-5 py-3'>
                 <Outlet />
             </main>
--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,8 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Cars from "./pages/Cars";
+import CreateCar from "./pages/CreateCar";
+import Car from "./pages/Car";
 
 const router = createBrowserRouter([
     {
-
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                path: "/cars",
+                element: <Cars />
+            },
+            {
+                path: "/new-car",
+                element: <CreateCar />
+            },
+            {
+                path: "/update-car/:id",
+                element: <CreateCar />
+            },
+            {
+                path: "/car/:id",
+                element: <Car />
+            }
+        ]
     }
 ])
 
