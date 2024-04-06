2bc927054db059c0d844b0409ddc38ea1ef2d175
Survivalx <goshaz7c@icloud.com>
2024-04-06 13:27:01 +0300

    create-car

--- react/src/pages/Car.jsx
+++ react/src/pages/Car.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
-const Car = () => {
+
+export default function Car() {
+    const [car, setCar] = useState(null)
+
+    const { id } = useParams()
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8001/api/cars/' + id)
+            .then(res => {
+                setCar(res.data.data)
+            })
+            .catch(error => console.error(error))
+    }, [])
     return (
-        <div>Car</div>
+        <div>
+            {
+                car &&
+                <div>
+                    <h1>{car.name}</h1>
+                    <p>Year: {car.year}</p>
+                    <p>Color: {car.color}</p>
+                    <p>Plate number: {car.number_plate}</p>
+                </div>
+            }
+        </div>
     )
 }
-
-export default Car
--- react/src/pages/CreateCar.jsx
+++ react/src/pages/CreateCar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { useParams } from 'react-router-dom'
 
-const CreateCar = () => {
+export default function CreateCar() {
+    const [car, setCar] = useState(null)
+    const { id } = useParams()
+    const nameRef = useRef()
+    const colorRef = useRef()
+    const yearRef = useRef()
+    const numberPlateRef = useRef()
+
+    const onSubmit = (ev) => {
+        ev.preventDefault()
+    }
+
     return (
-        <div>CreateCar</div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" ref={nameRef} placeholder='Name' />
+                <input type="text" ref={colorRef} placeholder='Color' />
+                <input type="text" ref={yearRef} placeholder='Year' />
+                <input type="text" ref={numberPlateRef} placeholder='Number Plate' />
+                <button>{{  }}</button>
+            </form>
+        </div>
     )
 }
 
-export default CreateCar
+
