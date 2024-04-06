2466c5d21135717d3fb82c9c2133167dc2994a59
Survivalx <goshaz7c@icloud.com>
2024-04-06 12:46:29 +0300

    react connection

--- react/src/pages/Cars.jsx
+++ react/src/pages/Cars.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Cars = () => {
+
+export default function Cars() {
+    const [cars, setCars] = useState(null);
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8001/api/cars")
+            .then(res => {
+                setCars(res.data)
+            })
+            .catch(error => {
+                console.error("Error fetching data")
+            })
+    }, [])
+
     return (
-        <div>Cars</div>
+        <div>
+            <h1 className='text-2xl font-bold'>Cars</h1>
+            {
+                cars &&
+                cars.map(car => (
+                    <div>
+                        <h3>
+                            {car.name}
+                        </h3>
+                        <p>{car.color}, {car.year}, {car.number_plate}</p>
+                    </div>
+                ))
+            }
+        </div>
     )
 }
 
-export default Cars
